e749e312845aa0d22355e94ff881479b1afd3e8e
Sofia <sofio.muradovi@gmail.com>
2022-10-19 14:36:56 +0400

    Rename method and add functionality to logout user if expired token error is returned

--- src/components/common/helperMethods.js
+++ src/components/common/helperMethods.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 
-export default function displayErrors(response, errorMessage) {
+export default async function handleErrors(response, errorMessage) {
     if (response.data.non_field_errors && response.data.non_field_errors[0]) {
         Vue.$toast.error(response.data.non_field_errors[0]);
+    } else if (response.data.expired_token_error) { //logout user when token is expired
+        Vue.$toast.error(response.data.expired_token_error[0]);
+        await store.dispatch("logoutUser");
+        await router.push({path: 'login'})
     } else {
         Vue.$toast.error(errorMessage);
     }
